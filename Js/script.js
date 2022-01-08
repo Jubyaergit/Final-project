16119f39e6a0e8c990e59f279e805b3260932e66
jubayer <mdprantoahmed512@gmail.com>
2022-01-08 15:12:39 +0600

    final project

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+$(".img-slider").slick({
+  arrows: false,
+  dots: true,
+  centerMode: true,
+  centerPadding: '0',
+});
+
+$('.venobox').venobox(); 
+
+$('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
